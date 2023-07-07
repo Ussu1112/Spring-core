e9232ab1ab296bd4dffd05f19aef3abcd096b4df
Ussu1112 <ussu1112@naver.com>
2023-07-07 17:03:17 +0900

    feat: 자동 빈 등록으로 변경 및 테스트

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,12 +1,14 @@
 package hello.core.member;
 
-import hello.core.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+//@Component("멤버 서비스 빈 이름 지정")
+@Component
 public class MemberServiceImpl implements MemberService {
-
-
     private final MemberRepository memberRepository;
 
+    @Autowired // ac.getbean(MemberRepository.class) 의 역할과 비슷하다 (getbean보다는 더 많은 역할을 지원한다.)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
